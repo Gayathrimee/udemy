4bf17763603e7ca970626960dab952909b9232e4
Gayathri <gayathrikrishna3132003@gmail.com>
2024-10-29 08:50:00 +0530

    jss

--- 5 Section Five/44 Custom Range Slider/version 3/script.js
+++ 5 Section Five/44 Custom Range Slider/version 3/script.js
@@ -50,7 +50,3 @@ range.addEventListener('input', (e)=>{
 const scale = (num, in_min, in_max, out_min, out_max) =>{
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
-
-
-
--- 5 Section Five/45 Netflix Mobile Navigation/version 3/script.js
+++ 5 Section Five/45 Netflix Mobile Navigation/version 3/script.js
@@ -1,11 +1,27 @@
+// const nav = document.querySelectorAll('.nav')
+// const closeBtn = document.querySelector('.close-btn')
+// const openBtn = document.querySelector('.open-btn')
+
+// closeBtn.addEventListener('click',()=>{
+//     nav.forEach(nav => nav.classList.remove('visible'))
+// })
+
+// openBtn.addEventListener('click',()=>{
+//     nav.forEach(nav => nav.classList.add('visible'))
+// })
+
+
+
+// ====================================
+
 const nav = document.querySelectorAll('.nav')
 const closeBtn = document.querySelector('.close-btn')
 const openBtn = document.querySelector('.open-btn')
 
-closeBtn.addEventListener('click',()=>{
+closeBtn.addEventListener('click', ()=>{
     nav.forEach(nav => nav.classList.remove('visible'))
 })
 
-openBtn.addEventListener('click',()=>{
+openBtn.addEventListener('click', ()=>{
     nav.forEach(nav => nav.classList.add('visible'))
-})
+})                                                           
